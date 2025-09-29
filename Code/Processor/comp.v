1aa64b830d0fb7ee1acfb86534df3ebad248a660
Draco_Malfoy <93931572+aman-sharma015@users.noreply.github.com>
2025-09-29 17:51:56 +0530

    Update comp.v

--- Code/Processor/comp.v
+++ Code/Processor/comp.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    01:49:38 02/13/2015 
-// Design Name: 
-// Module Name:    comp 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module comp(
 input [4:0]a,
 input [2:0]b,
