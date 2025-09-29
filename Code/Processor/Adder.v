deb7e2bd97a7af266638e1a56b9eecb12c3e2fdb
Draco_Malfoy <93931572+aman-sharma015@users.noreply.github.com>
2025-09-29 17:46:21 +0530

    Update Adder.v

--- Code/Processor/Adder.v
+++ Code/Processor/Adder.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:     
-// Design Name: 
-// Module Name:    Adder 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Adder(
     input [15:0] a,
     input [15:0] b,
