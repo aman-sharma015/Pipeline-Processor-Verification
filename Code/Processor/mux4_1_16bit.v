904a571d85e283037683b8639c17802659c5a82f
Draco_Malfoy <93931572+aman-sharma015@users.noreply.github.com>
2025-09-29 17:54:00 +0530

    Update mux4_1_16bit.v

--- Code/Processor/mux4_1_16bit.v
+++ Code/Processor/mux4_1_16bit.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    06:49:52 02/15/2015 
-// Design Name: 
-// Module Name:    mux4_1_16bit 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module mux4_1_16bit(
 input [15:0]a,b,c,d,
 input s0,s1,
