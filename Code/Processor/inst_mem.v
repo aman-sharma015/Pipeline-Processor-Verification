5666e1ada4656738f50ecc31d43083df30c495b0
Draco_Malfoy <93931572+aman-sharma015@users.noreply.github.com>
2025-09-29 17:53:26 +0530

    Update inst_mem.v

--- Code/Processor/inst_mem.v
+++ Code/Processor/inst_mem.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    22:03:35 02/14/2015 
-// Design Name: 
-// Module Name:    inst_mem 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module inst_mem(
 input [7:0]a,					//Memory Address
 output  [15:0]r					//Memory data output
