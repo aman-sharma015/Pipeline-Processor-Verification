0583bc3fd77fe86175cfe82c9c587a97a1c808b5
Draco_Malfoy <93931572+aman-sharma015@users.noreply.github.com>
2025-09-29 17:45:58 +0530

    Update ALU.v

--- Code/Processor/ALU.v
+++ Code/Processor/ALU.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    17:48:12 02/12/2015 
-// Design Name: 
-// Module Name:    ALU 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module ALU(
 input [15:0]a,
 input [15:0]b,
