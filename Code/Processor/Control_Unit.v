d32b11a98fb92f34c2d2b1bc6cfe3b123a440b85
Draco_Malfoy <93931572+aman-sharma015@users.noreply.github.com>
2025-09-29 17:46:36 +0530

    Update Control_Unit.v

--- Code/Processor/Control_Unit.v
+++ Code/Processor/Control_Unit.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    23:26:49 02/02/2015 
-// Design Name: 
-// Module Name:    Control_Unit 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Control_Unit(
 input [3:0]op,
 input ctrl,
