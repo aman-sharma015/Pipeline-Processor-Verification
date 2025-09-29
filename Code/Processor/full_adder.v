c8750c0281f4b3dfffa58c0fc868a8f7a30b4c91
Draco_Malfoy <93931572+aman-sharma015@users.noreply.github.com>
2025-09-29 17:52:34 +0530

    Update full_adder.v

--- Code/Processor/full_adder.v
+++ Code/Processor/full_adder.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    23:41:17 08/24/2014 
-// Design Name: 
-// Module Name:    full_adder 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module full_adder(
 	input a,b,cin,
 	output s,cout
